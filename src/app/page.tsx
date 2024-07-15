cf9d690ea13e7147d010f1366a614604c6f49c2c
Afiz Savage <savageafiz9@gmail.com>
2024-07-15 14:37:13 +0000

    complete services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ import logoNorthAdventures from '@/images/clients/north-adventures/logo-light.sv
 import logoPhobiaDark from '@/images/clients/phobia/logo-dark.svg'
 import logoPhobiaLight from '@/images/clients/phobia/logo-light.svg'
 import logoUnseal from '@/images/clients/unseal/logo-light.svg'
-import imageLaptop from '@/images/laptop.jpg'
+import imageFarm from '@/images/farm.jpg'
 import { type CaseStudy, type MDXEntry, loadCaseStudies } from '@/lib/mdx'
 import Photos from '@/components/Photos'
 
@@ -70,13 +70,13 @@ function CaseStudies({
   return (
     <>
       <SectionIntro
-        title="Harnessing technology for a brighter future"
+        title="Connecting small scale-farmers and producers to potential buyers"
         className="mt-24 sm:mt-32 lg:mt-40"
       >
         <p>
-          We believe technology is the answer to the world’s greatest
-          challenges. It’s also the cause, so we find ourselves in bit of a
-          catch 22 situation.
+          Depending on the digital equipment and digital aptitudes, users can
+          access marketplace information and contact each other via USSD and
+          SMS or through the online marketplace
         </p>
       </SectionIntro>
       <Container className="mt-16">
@@ -127,12 +127,12 @@ function Services() {
     <>
       <SectionIntro
         eyebrow="Services"
-        title="We help you identify, explore and respond to new opportunities."
+        title="Connecting small scale-farmers and producers to potential buyers"
         className="mt-24 sm:mt-32 lg:mt-40"
       >
         <p>
-          As long as those opportunities involve giving us money to re-purpose
-          old projects — we can come up with an endless number of those.
+          We help you access marketplace information and connect with market players
+          via USSD, SMS, or our online marketplace
         </p>
       </SectionIntro>
       <Container className="mt-16">
@@ -140,31 +140,26 @@ function Services() {
           <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
             <FadeIn className="w-[33.75rem] flex-none lg:w-[45rem]">
               <StylizedImage
-                src={imageLaptop}
+                src={imageFarm}
                 sizes="(min-width: 1024px) 41rem, 31rem"
                 className="justify-center lg:justify-end"
               />
             </FadeIn>
           </div>
           <List className="mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
-            <ListItem title="Web development">
-              We specialise in crafting beautiful, high quality marketing pages.
-              The rest of the website will be a shell that uses lorem ipsum
-              everywhere.
+            <ListItem title="Accessing market price information">
+              We collects price information from regional markets on a daily basis.
+              Our enumerators are collecting this information for bulk and retail
+              sales for more than 20 produces across Sierra Leone.
             </ListItem>
-            <ListItem title="Application development">
-              We have a team of skilled developers who are experts in the latest
-              app frameworks, like Angular 1 and Google Web Toolkit.
+            <ListItem title="Putting up produce for sale">
+              Create an advert for your product by selecting the crop,
+              quantity, and price. The listing and your necessary contact
+              information will be accessible to potential buyers.
             </ListItem>
-            <ListItem title="E-commerce">
-              We are at the forefront of modern e-commerce development. Which
-              mainly means adding your logo to the Shopify store template we’ve
-              used for the past six years.
-            </ListItem>
-            <ListItem title="Custom content management">
-              At Studio we understand the importance of having a robust and
-              customised CMS. That’s why we run all of our client projects out
-              of a single, enormous Joomla instance.
+            <ListItem title="Getting in touch with farmers">
+              Enquire about products for sale in a particular
+              region and obtain the contact information of the sellers advertising them.
             </ListItem>
           </List>
         </div>
@@ -175,7 +170,7 @@ function Services() {
 
 export const metadata: Metadata = {
   description:
-    'We are a development studio working at the intersection of design and technology.',
+    'We are a marketplace for connecting small scale-farmers and producers to potential buyers',
 }
 
 export default async function Home() {
@@ -195,13 +190,8 @@ export default async function Home() {
           </p>
         </FadeIn>
       </Container>
-
       <Photos />
-
-      <Clients />
-
-      <CaseStudies caseStudies={caseStudies} />
-
+      <Services />
       <Testimonial
         className="mt-24 sm:mt-32 lg:mt-40"
         client={{ name: 'Phobia', logo: logoPhobiaDark }}
@@ -210,9 +200,6 @@ export default async function Home() {
         finding a way to access the user’s microphone without triggering one of
         those annoying permission dialogs.
       </Testimonial>
-
-      <Services />
-
       <ContactSection />
     </>
   )
